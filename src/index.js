2278af8cf8368be0bcbe98649edaa8453dca4361
rigo0523 <limon1285@gmail.com>
2020-04-14 23:13:29 -0500

    updated App.js and imported PostsPage & SearchBar components onto app.js with my Tls hel

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+/* 
+Start here and then work through the 
+PostsContainer components and the CommentSection Components. 
+Not all files need code added. 
+Look at each file to see where you need to pass props or add code 
+*/
+
+// import the PostsPage and SearchBar and add them to the App
 
 ReactDOM.render(<App />, document.getElementById('root'));
